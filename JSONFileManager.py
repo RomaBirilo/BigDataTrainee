5f5f2649a6721eda3fde6bfeeb0f07af1d819e52
Roma Birilo <gkdichfjf@gmail.com>
2026-07-21 15:26:25 +0300

    Added writing into json files

--- JSONFileManager.py
+++ JSONFileManager.py
@@ -12,3 +12,15 @@ class JSONFileManager:
             raise Exception(f"Incorrect json syntaxis! Line:{error.lineno}")
         except Exception:
             raise Exception("Unknown error!")
+
+    @staticmethod
+    def write_json_file(path: str, data: list[dict] | dict) -> None:
+        try:
+            with open(path, 'w') as file:
+                json.dump(data, file, indent=4)
+        except FileNotFoundError:
+            raise Exception(f"Failed to create the file {path}!")
+        except TypeError as error:
+            raise Exception(f"Incorrect json syntaxis!{error}")
+        except Exception:
+            raise Exception("Unknown error!")
